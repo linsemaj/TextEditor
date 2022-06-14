6fe0d0ca172bba60a7e16e8ed4b0eb3b09c54a1f
James Lin <linsemaj@gmail.com>
2022-06-14 00:03:41 -0400

    add frame and text area

--- TextEditor.java
+++ TextEditor.java
@@ -1,8 +1,19 @@
 package com.example.text.editor;
 
+import java.awt.Frame;
+import java.awt.TextArea;
+
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
 public class TextEditor {
+	//window 
+	Frame frame= new JFrame("TextEditor");
+	JTextArea textArea= new JTextArea();
 	
-	
+	public TextEditor(){
+		
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
