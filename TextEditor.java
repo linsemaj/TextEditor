95d94eff08b5e16aab29c795fb7b4a6829974a37
James Lin <linsemaj@gmail.com>
2022-06-14 00:29:52 -0400

    made frame visible with text field

--- TextEditor.java
+++ TextEditor.java
@@ -1,23 +1,35 @@
 package com.example.text.editor;
 
-import java.awt.Frame;
-import java.awt.TextArea;
+import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JTextArea;
 
 public class TextEditor {
 	//window 
-	Frame frame= new JFrame("TextEditor");
+	JFrame frame= new JFrame("TextEditor");
+	//typing
 	JTextArea textArea= new JTextArea();
-	
+
 	public TextEditor(){
+		//MenuBar which is like frame but for menus
+		JMenuBar menuBar = new JMenuBar();
+		//menu of bar
+		JMenu fileMenu = new JMenu("File");
+		
+		menuBar.add(fileMenu);
+		frame.setSize(600, 500);
+		frame.add(textArea);
+		frame.setVisible(true);
 		
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		TextEditor t = new TextEditor();
+		System.out.println("HI");
 	}
 
 }
